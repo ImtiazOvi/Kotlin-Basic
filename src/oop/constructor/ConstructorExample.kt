ef27ecda4159ba160ebb30a3095b5c22db2e8f0f
imtiazOvi <imtiazuddin018@gmail.com>
2020-04-01 00:11:48 +0600

    Minor change in constructorExample class

--- src/oop/constructor/ConstructorExample.kt
+++ src/oop/constructor/ConstructorExample.kt
@@ -1,17 +1,16 @@
 package oop.constructor
 
 
+// This is primary constructor
+class ConstructorExample(var name: String, var age: Int) {
 
+    // This is primary constructor body
 
+}
+
+// This is main method
 fun main(args: Array<String>) {
     val constructorExample = ConstructorExample("Md Imtiaz Uddin", 25)
     println("First Name = ${constructorExample.name}")
     println("Age = ${constructorExample.age}")
-}
-
-// This is primary constructor
-class ConstructorExample( var name:String, var age:Int) {
-
-    // This is primary constructor body
-
 }
